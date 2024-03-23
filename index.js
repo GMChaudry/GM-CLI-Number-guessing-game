75d046ddd94fe410f1d74107131b13037edfad74
GMchaudry <g.m.chaudry@gmailc.om>
2024-03-23 23:54:19 +0500

    CLI NUMBER GUESSING GAME BY GM

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const input = await inquirer.prompt([
+    {
+        name: "PlayerGuessedNumber",
+        type: "number",
+        message: "Please Guess a Number Between 1-6",
+    }
+]);
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+if (input.PlayerGuessedNumber === randomNumber) {
+    console.log("Congrats You Got This Right");
+}
+else {
+    console.log("Sorry You Got This Wrong");
+}
+;
